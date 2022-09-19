1c722d7521a259c63ad6f61bf9b7f5872d878e6d
DESKTOP-VGDP77D\ADMIN <haivuong258@gmail.com>
2022-09-19 23:01:24 +0700

    thêm username,pw,isActive

--- src/main/java/com/greenvn/starlightelectronicsstore/entities/Employee.java
+++ src/main/java/com/greenvn/starlightelectronicsstore/entities/Employee.java
@@ -1,5 +1,6 @@
 package com.greenvn.starlightelectronicsstore.entities;
 
+
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
@@ -12,7 +13,12 @@ public class Employee extends Person{
 	@ManyToOne
 	@NotBlank(message = "Chức vụ không được để trống!")
 	private Position position;
-
+	
+	private String userName;
+	private String password;
+	private Boolean isActive = false;
+	
+	
 	public Position getPosition() {
 		return position;
 	}
@@ -20,5 +26,30 @@ public class Employee extends Person{
 	public void setPosition(Position position) {
 		this.position = position;
 	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Boolean getIsActive() {
+		return isActive;
+	}
+
+	public void setIsActive(Boolean isActive) {
+		this.isActive = isActive;
+	}
+	
 	
 }
