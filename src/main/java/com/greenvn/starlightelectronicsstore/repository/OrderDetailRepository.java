49f9f617cb59befd5d32832efd39695fa7693c46
DESKTOP-VGDP77D\ADMIN <haivuong258@gmail.com>
2022-09-20 10:53:50 +0700

    indexShop,layout

--- src/main/java/com/greenvn/starlightelectronicsstore/repository/OrderDetailRepository.java
+++ src/main/java/com/greenvn/starlightelectronicsstore/repository/OrderDetailRepository.java
@@ -1,11 +1,11 @@
-package com.greenvn.starlightelectronicsstore.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import com.greenvn.starlightelectronicsstore.entities.OrderDetail;
-
-@Repository
-public interface OrderDetailRepository extends JpaRepository<OrderDetail,Integer>{
-
-}
+//package com.greenvn.starlightelectronicsstore.repository;
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Repository;
+//
+//import com.greenvn.starlightelectronicsstore.entities.OrderDetail;
+//
+//@Repository
+//public interface OrderDetailRepository extends JpaRepository<OrderDetail,Integer>{
+//
+//}
